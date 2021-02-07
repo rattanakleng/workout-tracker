a84cb2cddb7f391a97032b5a1e595a83ea905fb0
Rattanak Leng <rleng@live.com>
2021-02-06 21:47:24 -0800

    create models folder and add index.js and workout.js

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+//Dependecies
+const mongoose = require("mongoose");
+
+const workoutSchema = new Schema (
+    {
+        day: {
+            type: Date,
+            defualt: () => new Date()
+        },
+
+        exercise: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Exercise type"
+                },
+
+                name: {
+                    type:String,
+                    trim: true,
+                    required: "Exercise name"
+                },
+
+                duration: {
+                    type: Number,
+                    required: "Duration in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    },
+);
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+
+module.exports = Workout;
+
+
+
