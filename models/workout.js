fde71d269708deec5813da9dcb8a566145097540
Rattanak Leng <rleng@live.com>
2021-02-06 22:07:25 -0800

    fix error  define Schema

--- models/workout.js
+++ models/workout.js
@@ -1,6 +1,8 @@
 //Dependecies
 const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
 const workoutSchema = new Schema (
     {
         day: {
@@ -44,7 +46,6 @@ const workoutSchema = new Schema (
 );
 
 const Workout = mongoose.model('Workout', workoutSchema);
-
 
 module.exports = Workout;
 
